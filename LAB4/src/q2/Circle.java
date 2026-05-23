a5072f8c4283f65ac1d8846569e553348f44cc13
karushkarushman-alt <karushkarushman@gmail.com>
2026-05-23 20:39:36 +0530

    Temperature.java

--- LAB4/src/q2/Circle.java
+++ LAB4/src/q2/Circle.java
@@ -1,5 +1,19 @@
 package q2;
+import java.util.Scanner;
 
-public class Circle {
+class Temperature {
+    private double fahrenheit;
+    private double celsius;
 
+    // Method to set Fahrenheit temperature
+    public void setFahrenheit(double fahrenheit) {
+        this.fahrenheit = fahrenheit;
+        this.celsius = (fahrenheit - 32) * 5 / 9;
+    }
+
+    // Method to get Celsius temperature
+    public double getCelsius() {
+        return celsius;
+    }
 }
+
